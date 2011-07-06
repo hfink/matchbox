e75b7c0da0cdaa7c8eb99ee71d8b29f4baf8996d
Heinrich Fink <hf@hfink.eu>
2011-07-06 17:20:04 +0200

    Adding MIT license headers to newly added source files.

--- Xcode/iPodLibMFCCTest/iPodLibMFCCTestTests/IPSongReaderTest.h
+++ Xcode/iPodLibMFCCTest/iPodLibMFCCTestTests/IPSongReaderTest.h
@@ -1,11 +1,22 @@
+//Copyright (c) 2011 Heinrich Fink hf@hfink.eu
 //
-//  IPSongReaderTest.h
-//  iPodLibMFCCTest
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
 //
-//  Created by Heinrich Fink on 5/22/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
 //
-//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
 
 //  Application unit tests contain unit test code that must be injected into an application to run correctly.
 //  Define USE_APPLICATION_UNIT_TEST to 0 if the unit test code is designed to be linked into an independent test executable.
